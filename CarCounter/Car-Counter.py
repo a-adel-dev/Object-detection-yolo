31f030f3f69b8dfa36d3ee70d9f624be7e087047
Ahmad A. Fouad <a.adel.dev@gmail.com>
2023-10-06 21:45:16 +0300

    car-counter start

--- CarCounter/Car-Counter.py
+++ CarCounter/Car-Counter.py
@@ -3,9 +3,7 @@ import cv2
 import cvzone
 import math
 
-# cap = cv2.VideoCapture(0) # for webcam
-# cap.set(3,1280)
-# cap.set(4,720)
+
 
 cap = cv2.VideoCapture("../videos/cars.mp4") # for video
 
@@ -34,16 +32,21 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             w, h = x2-x1, y2-y1
-            cvzone.cornerRect(img,(x1, y1, w, h))
+
             # confidence
             conf = math.ceil((box.conf[0]*100))/100
 
             #Class Name
             cls = int(box.cls[0])
 
-            cvzone.putTextRect(img, f'{classNames[cls]} - {conf}', (max(0, x1), max(40, y1)), scale=0.9, thickness=1)
+            currentClass = classNames[cls]
+            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike"\
+                    and conf > 0.3:
+                cvzone.putTextRect(img, f'{currentClass} - {conf}', (max(0, x1), max(40, y1)), scale=0.6,
+                               thickness=1, offset=3)
+                cvzone.cornerRect(img, (x1, y1, w, h), l=5)
 
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
 
